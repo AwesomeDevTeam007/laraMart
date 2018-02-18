ac535b08effdeeb75aedc82534efe085a817084c
Nay Lin Aung <joyboy@Nays-MacBook-Pro.local>
2018-02-18 16:02:40 +0630

    some updates

--- resources/views/backend/product_inventories/fields.blade.php
+++ resources/views/backend/product_inventories/fields.blade.php
@@ -7,7 +7,7 @@
 <!-- Branch Id Field -->
 <div class="form-group col-sm-12">
     <!-- {!! Form::label('branch_id', 'Branch Id:') !!} -->
-    <!-- {!! Form::number('branch_id', null, ['class' => 'form-control']) !!} -->
+     {!! Form::hidden('branch_id[]', $branch->id, ['class' => 'form-control']) !!} 
      <h3> {!! $branch->name !!}</h3>
 </div>
 
